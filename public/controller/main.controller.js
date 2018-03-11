33f3cc130be70ce9c22a5e39e759aabc13aab3fc
Max <maxwiese13@gmail.com>
2018-03-11 19:56:22 +0100

    add Button

--- public/controller/main.controller.js
+++ public/controller/main.controller.js
@@ -2,7 +2,11 @@ var myApp = angular.module('myApp');
 
 myApp.controller('SystemController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
   console.log("Controllers loadet ... ");
-
+  $scope.runRadio = function() {
+    http.get('/run',).success(response) =>{
+      console.log('run Radio');
+    };
+  };
 
 
 
